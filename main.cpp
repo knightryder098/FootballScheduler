a0a9bbdac4bbb0710064017b0f7c35becf1c5ab1
gobinda lugun <gobinknightryder098@gmail.com>
2021-12-20 18:16:34 +0530

    updates showdetails()

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <string.h>
 #include <algorithm>
 using namespace std;
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 
 struct Team // user defined data structure for storing all the details.
 {
@@ -21,20 +24,23 @@ char ScharSet[] = "abcdefghijklmnopqrstuvwxyz"; // used to create random names
 char CcharSet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // used to create random names
 Team TeamBook[16];                              // Global Variable for storing all details about teams
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 // randomizer function
 void randomizer(Team *A, int pos)
 {
   A->key = pos;
   char name[10];
-  int index = (rand() % 26) + 1;
+  int index = (rand() % 25) + 1;
   name[0] = CcharSet[index];
   for (int i = 1; i < 10; i++)
   {
-    index = (rand() % 26) + 1;
+    index = (rand() % 25) + 1;
     name[i] = ScharSet[index];
   }
   strcpy(A->name, name);
-  A->strongPlayer = (rand() % 11) + 1;
+  A->strongPlayer = (rand() % 9) + 1;
   A->weakPlayer = 11 - A->strongPlayer;
   A->teamOpinion = (double)(A->strongPlayer * 100) / 11;
   A->publicOpinion = (double)((rand() % 100) + 1);
@@ -42,16 +48,22 @@ void randomizer(Team *A, int pos)
   A->matchesLost = 0;
   return;
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 // function to enter details by yourself
 void entryYourself(int size)
 {
   for (int i = 0; i < size; i++)
   {
+    char name[10];
     system("cls");
     TeamBook[i].key = i + 1;
     printf("Enter details of %d team mmenber\n");
     printf("\tEnter the name of team\n");
-    scanf(" %s", &TeamBook[i].name);
+    scanf(" %s", &name);
+    strcpy(TeamBook[i].name, name);
     printf("\tEnter no of string players\n");
     scanf(" %d", &TeamBook[i].strongPlayer);
     printf("\tEnter no Public Opinion \n");
@@ -103,10 +115,16 @@ void enterDetails()
     printf("Can't Scheduled matches for %d numbers of teams\n", a);
   return;
 }
-void displayDetails()
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+void displayDetails(Team a[])
 {
+  int teams = 0;
+
   printf("SL.no\tName\t\t\tSP\tWP\tTO\tPO\tMW\tML\n");
-  for (int i = 0; *TeamBook[i].name != '\0'; i++)
+  for (int i = 0; *a[i].name != '\0'; i++)
   {
     printf("%d\t%s\t\t%d\t%d\t%0.2f\t%0.2f\t%d\t%d", // to display only 2point, we use %0.2f
            TeamBook[i].key + 1,
@@ -121,76 +139,132 @@ void displayDetails()
   }
   getch();
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 // sorting function and its required functions
 int sortbyplayer(const void *p, const void *q)
 {
   return ((struct Team *)p)->strongPlayer < ((struct Team *)q)->strongPlayer;
 }
 
-////////////////////////////////////////////////////////////////////////////
-// void DisplayResults();
-Team match(Team a, Team b)
+int sortbyteamOpinion(const void *p, const void *q)
 {
-  // return winner of both teams based on their properties
-  Team wwinner;
-  float winpercentageA = (a.teamOpinion + a.publicOpinion);
-  float winpercentageB = (b.publicOpinion + b.teamOpinion);
-  float losepercentageA = (rand() % 200) + 1;
-  float losepercentageB = (rand() % 200) + 1;
+  return ((struct Team *)p)->teamOpinion < ((struct Team *)q)->teamOpinion;
+}
+// Team match(Team a, Team b)
+// {
+//   // return winner of both teams based on their properties
+//   Team wwinner;
+//   float winpercentageA = (a.teamOpinion + a.publicOpinion);
+//   float winpercentageB = (b.publicOpinion + b.teamOpinion);
+//   float losepercentageA = (rand() % 200) + 1;
+//   float losepercentageB = (rand() % 200) + 1;
 
-  if (wi)
-}
-void matchMakerKnockout(Team a[], int size)
+//   if (winpercentageA > winpercentageB && losepercentageA < losepercentageB)
+//   {
+//     a.matchesWon++;
+//     a.teamOpinion += (a.teamOpinion * 0.02);
+//     a.publicOpinion += (a.publicOpinion * 0.02);
+//     wwinner = a;
+//   }
+//   else if (losepercentageA < losepercentageB)
+//   {
+//     a.matchesWon++;
+//     a.teamOpinion += (a.teamOpinion * 0.02);
+//     a.publicOpinion += (a.publicOpinion * 0.02);
+//     wwinner = a;
+//   }
+//   else
+//   {
+//     b.matchesWon++;
+//     b.teamOpinion += (b.teamOpinion * 0.02);
+//     b.publicOpinion += (b.publicOpinion * 0.02);
+//     wwinner = b;
+//   }
+//   return wwinner;
+// }
+// void matchMakerKnockout(Team a[], int size, bool flag)
+// {
+//   // Knockout Matches
+//   if (size == 1)
+//   {
+//     return;
+//   }
+//   int teams = 0;
+//   for (teams; *a[teams].name != '\0'; teams++)
+//     ;
+//   qsort(a, teams, sizeof(Team), sortbyplayer);
+//   int roundNumber = 4 - log2(size);
+//   int i;
+//   if (flag)
+//     i = 1;
+//   else
+//     i = 0;
+//   int currpos = 0;
+//   Team currentRound[size / 2];
+//   for (i; i < size; i += 2)
+//   {
+//     currentRound[currpos] = match(a[i], a[i + 1]);
+//   }
+//   printf("\t%d ROUND\n*****************\n", roundNumber + 1);
+//   displayDetails(currentRound);
+// }
+
+// void schedulerMatches()
+// {
+//   bool flag = false;
+//   int teams = 0;
+//   for (teams; *TeamBook[teams].name != '\0'; teams++)
+//     ;
+//   qsort(TeamBook, teams, sizeof(Team), sortbyplayer);
+//   if (teams % 2 == 0)
+//   {
+//     flag = false;
+//     // even no of teams
+//     // printf("\t As the number of teams are even and power of Two\n\tMatch TYPE: KNOCKOUT\n");
+//     matchMakerKnockout(TeamBook, teams, flag);
+//   }
+//   else
+//   {
+//     // odd number of teams
+//     flag = true;
+//     matchMakerKnockout(TeamBook, teams, flag);
+//   }
+//   // getch();
+// }
+
+void match(Team *a, Team *b)
 {
-  // Knockout Matches
-  int roundNumber = 4 - log2(size);
-  if (size == 1)
-    return;
-  int currpos = 0;
-  Team currentRound[size / 2];
-  for (int i = 0; i < size; i += 2)
-  {
-    currentRound[currpos] = match(a[i], a[i + 1]);
-  }
-  printf("%d ROUND\n*****************\n", roundNumber + 1);
-  printf("\tName\t\t\tSP\tWP\tTO\tPO\tMW\tML\n");
-  for (int i = 0; i < currpos; i++)
-  {
-    printf("\t%s\t\t%d\t%d\t%0.2f\t%0.2f\t%d\t%d", // to display only 2point, we use %0.2f
-           currentRound[i].name,
-           currentRound[i].strongPlayer,
-           currentRound[i].weakPlayer,
-           currentRound[i].teamOpinion,
-           currentRound[i].publicOpinion,
-           currentRound[i].matchesWon,
-           currentRound[i].matchesLost);
-    printf("\n");
-  }
 }
 
-void matchMakerLeague(Team a[])
-{
-  // Leauge matches
-}
-void schedulerMatches()
+void schedulerMatches(Team *Current[])
 {
   int teams = 0;
-  for (teams; *TeamBook[teams].name != '\0'; teams++)
+  for (teams; *Current[teams].name != '\0'; teams++)
     ;
-  qsort(TeamBook, teams, sizeof(Team), sortbyplayer);
+  qsort(Current, teams, sizeof(Team), sortbyteamOpinion);
   if (teams % 2 == 0)
   {
-    // even no of teams
-    printf("\t As the number of teams are even and power of Two\n\tMatch TYPE: KNOCKOUT\n");
-    matchMakerKnockout(TeamBook, teams);
+    // even
+    printf("\tAs Even number of teams are present , no BYE is given!!!\n");
+    for (int i = 0; i < teams / 2; i++)
+    {
+      printf("\tALL ROUND %d MATCHES\n", i + 1);
+      {
+      }
+    }
   }
   else
   {
-    // odd number of teams
-    matchMakerLeague(TeamBook);
+    // odd
   }
-  getch();
 }
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 int main()
 {
   while (1)
@@ -201,8 +275,7 @@ int main()
     printf("\t\tCHOICES\t\t\n");
     printf("\t1.Enter Teams Details : Type \'1\'\n ");
     printf("\t2.Display Teams Details : Type \'2\'\n ");
-    printf("\t3.Start matching 0f Teams : Type \'3\'\n ");
-    printf("\t4.Results of Scheduled Matches : Type \'4\'\n ");
+    printf("\t3.Schedule and view WINNER : Type \'3\'\n ");
     scanf("%d", &choice);
     switch (choice)
     {
@@ -210,15 +283,13 @@ int main()
       enterDetails();
       break;
     case 2:
-      displayDetails();
-      break;
+    {
+      displayDetails(TeamBook);
+    }
+    break;
     case 3:
-      // printf("Hello\n");
-      schedulerMatches();
-      break;
-    case 4:
       printf("Hello\n");
-      // DisplayResults();
+      schedulerMatches(&TeamBook);
       break;
     default:
     {
@@ -230,3 +301,6 @@ int main()
   }
   return 0;
 }
+
+///////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
